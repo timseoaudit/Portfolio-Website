def31b0fee05b0a42bf646d2b01188a908deb034
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-28 05:07:48 +0100

    try to add some jS

---
+++ resources/JS/main.js
@@ -0,0 +1,6 @@
+let p1Pic = document.getElementById('p1pic');
+let projectPic = document.getElementById('projectpic1');
+
+p1Pic.addEventListener('click', function() {
+    projectPic.style.display = 'block';
+});
