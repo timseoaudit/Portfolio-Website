fe928e9a3f20d1cd2468037e6e254d95e43e9d8d
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-28 05:13:50 +0100

    let to con

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,5 +1,5 @@
-let p1Pic = document.getElementById('p1pic');
-let projectPic = document.getElementById('projectpic1');
+const p1Pic = document.getElementById('p1pic');
+const projectPic = document.getElementById('projectpic1');
 
 p1Pic.onclick = function() {
     projectPic.style.display = 'block';
