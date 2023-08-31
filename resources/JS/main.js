49a065c7621aa0f3930b3d9586e156801c3e5871
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-31 05:15:11 +0100

    add JS for Project 2 and 3 and CSS for same

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,8 +1,24 @@
+// First Project
 const p1Pic = document.getElementById('p1pic');
-const projectPic = document.getElementById('projectpic1');
+let projectPic1 = document.getElementById('projectpic1');
 
 p1Pic.onclick = function() {
-    projectPic.style.display = 'block';
+    projectPic1.style.display = 'block';
 }
 
+// Second Project
+const p2Pic = document.getElementById('p1pic');
+let projectPic2 = document.getElementById('projectpic1');
+
+p2Pic.onclick = function() {
+    projectPic2.style.display = 'block';
+}
+
+// Third Project
+const p3Pic = document.getElementById('p1pic');
+let projectPic3 = document.getElementById('projectpic1');
+
+p3Pic.onclick = function() {
+    projectPic3.style.display = 'block';
+}
 // need to learn how to use this one and how to implement JS properly not happy 
