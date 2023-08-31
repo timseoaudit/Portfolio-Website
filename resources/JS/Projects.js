1fb6768f31a89f62e223e2dd7e9c4eaaa64c10ce
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-31 06:10:04 +0100

    comments added for JS

--- resources/JS/Projects.js
+++ resources/JS/Projects.js
@@ -1,23 +1,23 @@
 // First Project
-const p1Pic = document.getElementById('p1pic');
-let projectPic1 = document.getElementById('projectpic1');
-let p1Close = document.getElementById('p1close');
+const p1Pic = document.getElementById('p1pic'); //define the varible to the id of the image
+let projectPic1 = document.getElementById('projectpic1'); //define the varible to the id opening tag of the image
+let p1Close = document.getElementById('p1close'); //define the varible to the id of the closing tag of the image 
 
-p1Pic.onclick = function() {
-    projectPic1.style.display = 'block';
-    p1Pic.style.display = 'none';
-    p1Close.style.display = 'block';
+p1Pic.onclick = function() { // define a function that will run when the text is clicked
+    projectPic1.style.display = 'block'; // change the display of the image to block
+    p1Pic.style.display = 'none'; // change the display of the text to none
+    p1Close.style.display = 'block'; // change the display of the closing tag to block
 }
 
-p1Close.onclick = function() {
-    projectPic1.style.display = 'none';
-    p1Pic.style.display = 'block';
-    p1Close.style.display = 'none';
+p1Close.onclick = function() { // another function that will run when the closing tag is clicked
+    projectPic1.style.display = 'none'; // change the display of the image to none
+    p1Pic.style.display = 'block'; // change the display of the text to block
+    p1Close.style.display = 'none'; // change the display of the closing tag to none
 }
-
+// this copied two more times 
 
 // Second Project
-const p2Pic = document.getElementById('p2pic');
+const p2Pic = document.getElementById('p2pic'); 
 let projectPic2 = document.getElementById('projectpic2');
 let p2Close = document.getElementById('p2close');
 
