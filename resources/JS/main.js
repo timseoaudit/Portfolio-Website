6e2b30b747b2e5c8466a131c1daeb21220bd775e
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-08-31 05:38:14 +0100

    add JS for Project 2

--- resources/JS/main.js
+++ resources/JS/main.js
@@ -1,30 +1,36 @@
 // First Project
 const p1Pic = document.getElementById('p1pic');
 let projectPic1 = document.getElementById('projectpic1');
+let p1Close = document.getElementById('p1close');
 
 p1Pic.onclick = function() {
     projectPic1.style.display = 'block';
-    p1Pic.innerHTML = 'Click to close';
+    p1Pic.style.display = 'none';
+    p1Close.style.display = 'block';
 }
 
-p1Pic.onclick = function() {
+p1Close.onclick = function() {
     projectPic1.style.display = 'none';
-    p1Pic.innerHTML = 'Picture of the Cool Currents Landing Page';
+    p1Pic.style.display = 'block';
+    p1Close.style.display = 'none';
 }
+
 
 // Second Project
 const p2Pic = document.getElementById('p2pic');
 let projectPic2 = document.getElementById('projectpic2');
+let p2Close = document.getElementById('p2close');
 
 p2Pic.onclick = function() {
     projectPic2.style.display = 'block';
+    p2Pic.style.display = 'none';
+    p2Close.style.display = 'block';
+}
+
+p2Close.onclick = function() {
+    projectPic2.style.display = 'none';
+    p2Pic.style.display = 'block';
+    p2Close.style.display = 'none';
 }
 
 // Third Project
-const p3Pic = document.getElementById('p1pic');
-let projectPic3 = document.getElementById('projectpic1');
-
-p3Pic.onclick = function() {
-    projectPic3.style.display = 'block';
-}
-// need to learn how to use this one and how to implement JS properly not happy 
