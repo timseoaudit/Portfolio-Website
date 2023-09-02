9bf1edbe520c69ebfbb5a46af7a4676904e3ea00
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-02 05:31:43 +0100

    add JS for Contactform

---
+++ resources/JS/ContactForm.js
@@ -0,0 +1,6 @@
+const faSolid = document.getElementById('contactenv');
+const contactForm = document.getElementById('contactform');
+
+faSolid.onclick = function() {
+    contactForm.style.display = 'block';
+}
